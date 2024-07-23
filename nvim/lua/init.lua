03d9269ee968cb1751e91ea87dbf9fd4e37ea08a
Sangho Lee <sangholee45@outlook.com>
2024-07-24 00:38:59 +0900

    Change nvim-tree mapping
    Signed-off-by: Sangho Lee <sangholee45@outlook.com>

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -43,8 +43,8 @@ local function my_on_attach(bufnr)
   api.config.mappings.default_on_attach(bufnr)
 
   -- custom mappings
-  vim.keymap.set('n', '<F11>', api.tree.change_root_to_node, opts('CD'))
-  vim.keymap.set('n', '<F12>',  api.tree.change_root_to_parent, opts('Up'))
+  vim.keymap.set('n', 'cc', api.tree.change_root_to_node, opts('CD'))
+  --vim.keymap.set('n', '<F12>',  api.tree.change_root_to_parent, opts('Up'))
   vim.keymap.set('n', '?',  api.tree.toggle_help, opts('Help'))
 end
 
