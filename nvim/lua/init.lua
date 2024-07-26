9ba5a95557b7ccd2dc03092a3420af7c73859dbf
Sangho Lee <sangholee45@outlook.com>
2024-07-26 20:12:23 +0900

    Add code completion feature
    Signed-off-by: Sangho Lee <sangholee45@outlook.com>

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -17,6 +17,13 @@ vim.call('plug#begin')
 Plug('catppuccin/nvim', { ['as'] = 'catppuccin' })
 Plug('lewis6991/gitsigns.nvim')
 Plug('lukas-reineke/indent-blankline.nvim')
+Plug('hrsh7th/cmp-buffer')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-vsnip')
+Plug('hrsh7th/vim-vsnip')
 Plug('nvim-tree/nvim-web-devicons')
 Plug('nvim-tree/nvim-tree.lua')
 Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
@@ -53,7 +60,7 @@ local function my_on_attach(bufnr)
   vim.keymap.set('n', '?',  api.tree.toggle_help, opts('Help'))
 end
 
-require("nvim-tree").setup({
+require("nvim-tree").setup {
   on_attach = my_on_attach,
   respect_buf_cwd = true,
   sort = {
@@ -69,7 +76,7 @@ require("nvim-tree").setup({
   filters = {
     dotfiles = true,
   },
-})
+}
 
 require'nvim-treesitter.configs'.setup {
   auto_install = true,
@@ -96,6 +103,49 @@ require("lspconfig").clangd.setup {}
 require("lspconfig").cmake.setup {}
 require("lspconfig").pyright.setup {}
 require("lspconfig").rust_analyzer.setup {}
+local cmp = require("cmp")
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+  },
+}
+
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  }),
+  matching = { disallow_symbol_nonprefix_matching = false }
+})
 
 -----------------------
 -- Mapping
