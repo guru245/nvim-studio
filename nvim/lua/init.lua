2e2108b39e02cd6175e1a7b243edc92b1b3f294d
Sangho Lee <sangho.lee@amlogic.com>
2024-09-10 06:59:13 +0800

    Add Gitsign mappings
    Change-Id: I781906fb647c53ec4b9099c17f65e19959e0b5ee
    Signed-off-by: Sangho Lee <sangho.lee@amlogic.com>

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -220,6 +220,19 @@ require('gitsigns').setup {
       end
     end)
 
+    -- Actions
+    map('n', '<leader>hs', gitsigns.stage_hunk)
+    map('n', '<leader>hr', gitsigns.reset_hunk)
+    map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', '<leader>hS', gitsigns.stage_buffer)
+    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+    map('n', '<leader>hR', gitsigns.reset_buffer)
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+    map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+    map('n', '<leader>td', gitsigns.toggle_deleted)
+
   end
 }
 
